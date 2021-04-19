8fccfe1a0ada8bef7e25db2f747e2846bddf117d
Kevin Shen <kshen@pod-login1.podcluster>
2021-04-19 05:41:20 -0700

    Minimal setup.py, use with pip install -e

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name = 'sim_dev',
+      version = '0.1dev',
+      description = 'my working sim distribution',
+      packages = ['sim','utility'],
+      author = 'Kevin Shen',
+      author_email = 'kevin.shen@ucsb.edu'
+     )
